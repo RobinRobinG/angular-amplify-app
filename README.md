e9948cd219c0fd35b7748ccbbfa0df31e0b5ba2d
Robin Guan <robinguan@MacBook-Pro.local>
2020-04-17 15:12:40 -0700

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# TestApp8
-
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.26.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, AuthComponent, ProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, AmplifyAngularModule, MDBBootstrapModule.forRoot(), FormsModule],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
