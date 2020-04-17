eed46d779b56afc2f6486ff47defdc78dbc3e974
Robin Guan <robinguan@MacBook-Pro.local>
2020-04-17 15:48:44 -0700

    updated amplify settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ProfileComponent } from './profile/profile.component';
-import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+
+import { AmplifyAngularModule, AmplifyService, AmplifyModules } from 'aws-amplify-angular';
+import Auth from '@aws-amplify/auth';
+import Interactions from '@aws-amplify/interactions';
+import Storage from '@aws-amplify/storage';
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [AppComponent, AuthComponent, ProfileComponent],
   imports: [BrowserModule, AppRoutingModule, AmplifyAngularModule, MDBBootstrapModule.forRoot(), FormsModule],
-  providers: [AmplifyService],
+  providers: [
+    {
+      provide: AmplifyService,
+      useFactory: () => {
+        return AmplifyModules({
+          Auth,
+          Storage,
+          Interactions
+        });
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
